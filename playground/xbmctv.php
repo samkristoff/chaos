f2c1ce5ae19623f5433a508c8124eadaf01366a0
Sammy_K <samkristoff@gmail.com>
2014-02-15 16:32:01 -0600

    Update xbmctv.php

--- playground/xbmctv.php
+++ playground/xbmctv.php
@@ -1,9 +1,5 @@
 <?php
 
-//phpinfo();
-
-echo 'XBMC!';
-
 $db = new SQLite3('/home/xbian/.xbmc/userdata/Database/MyVideos75.db');
 
 $results = $db->query('SELECT C00 FROM tvshow');
@@ -11,4 +7,5 @@ $results = $db->query('SELECT C00 FROM tvshow');
 while ($row = $results->fetchArray()) {
     var_dump($row);
 }
+
 ?>
